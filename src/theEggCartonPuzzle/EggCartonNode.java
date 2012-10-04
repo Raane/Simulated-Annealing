5413de7a71417276c55a7942ac09dba306fb0114
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 14:35:48 +0200

    Adding getTarget.

--- src/theEggCartonPuzzle/EggCartonNode.java
+++ src/theEggCartonPuzzle/EggCartonNode.java
@@ -6,6 +6,8 @@ import generalSimulatedAnnealing.Node;
 
 public class EggCartonNode implements Node{
 
+	private final int K=2, M=5, N=5;
+	
 	public static EggCartonNode getP() {
 		return new EggCartonNode();
 	}
@@ -32,6 +34,12 @@ public class EggCartonNode implements Node{
 	public Node getBestNeighbour() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public double getTarget() {
+		// TODO Auto-generated method stub
+		return Math.min(M,N)*K; // This is the optimal solution of the problem
 	}
 
 }
