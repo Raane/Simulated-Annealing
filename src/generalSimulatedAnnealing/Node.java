ac3c88c171de124896999ad0409703e7e236a359
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 09:17:14 +0200

    Adding methods for treating neighbours.

--- src/generalSimulatedAnnealing/Node.java
+++ src/generalSimulatedAnnealing/Node.java
@@ -1,9 +1,14 @@
 package generalSimulatedAnnealing;
 
+import java.util.ArrayList;
+
 public interface Node extends Comparable{
 
 	public int compareTo(Object arg0); // It's simple to find the best child when I can compare them :D
 
 	public double getObjectiveFunction(); // This is the objective function of the nodes
+
+	public ArrayList<Node> getNeighbours();
 	
+	public Node getBestNeighbour();
 }
