e92beb339327bc3ed24a7e6da336f8b7f6119b3c
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 09:14:02 +0200

    Adding an objective function.

--- src/generalSimulatedAnnealing/Node.java
+++ src/generalSimulatedAnnealing/Node.java
@@ -4,6 +4,6 @@ public interface Node extends Comparable{
 
 	public int compareTo(Object arg0); // It's simple to find the best child when I can compare them :D
 
-	
+	public double getObjectiveFunction(); // This is the objective function of the nodes
 	
 }
