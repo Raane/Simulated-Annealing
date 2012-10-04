effcc8f673198d869c6239628f3c29b6a502bf16
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 14:51:42 +0200

    Implementing the general simulated annealing algorithm.

--- src/generalSimulatedAnnealing/SimulatedAnnealing.java
+++ src/generalSimulatedAnnealing/SimulatedAnnealing.java
@@ -4,7 +4,6 @@ public class SimulatedAnnealing {
 	
 	private final Node P;
 	private final double T_MAX, DT;
-	private double t;
 	
 	public SimulatedAnnealing(Node P, double T_MAX, double DT) {
 		this.P = P;
@@ -16,9 +15,19 @@ public class SimulatedAnnealing {
 		return solve(P,T_MAX);
 	}
 
-	private Node solve(Node p2, double t_MAX2) {
-		return null;
+	private Node solve(Node P, double T) {
+		double fp = P.getObjectiveFunction();
+		if(fp>=P.getTarget()) {
+			return P;
+		}
+		Node PMax = P.getBestNeighbour();
+		double q = (PMax.getObjectiveFunction()-fp)/2;
+		double p = Math.min(1,Math.exp(-q/T));
+		double x = Math.random();
+		if(x>p) {
+			return solve(PMax, T-DT);
+		} else {
+			return solve(P.getRandomNeighbour(), T-DT);
+		}
 	}
-	
-	
 }
