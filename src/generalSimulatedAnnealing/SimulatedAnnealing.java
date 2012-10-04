5797c8ad3c7dbc08b0cd3ef19db9c03de4a601f6
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 10:10:54 +0200

    Adding a solve method

--- src/generalSimulatedAnnealing/SimulatedAnnealing.java
+++ src/generalSimulatedAnnealing/SimulatedAnnealing.java
@@ -1,7 +1,24 @@
 package generalSimulatedAnnealing;
 
-import theEggCartonPuzzle.EggCartonNode;
-
 public class SimulatedAnnealing {
-	Node P = EggCartonNode.getP();
+	
+	private final Node P;
+	private final double T_MAX, DT;
+	private double t;
+	
+	public SimulatedAnnealing(Node P, double T_MAX, double DT) {
+		this.P = P;
+		this.T_MAX = T_MAX;
+		this.DT = DT;
+	}
+	
+	public Node solve() {
+		return solve(P,T_MAX);
+	}
+
+	private Node solve(Node p2, double t_MAX2) {
+		return null;
+	}
+	
+	
 }
